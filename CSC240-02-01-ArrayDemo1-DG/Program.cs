cba6114d534ebba6df05d9c200183d5b96b68813
Owner <Owner@DESKTOP-5OQMRF9>
2024-07-05 18:26:52 -0400

    Add project files.

---
+++ CSC240-02-01-ArrayDemo1-DG/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+class ArrayDemo1
+{
+    static void Main()
+    {
+        double[] payRate = { 12.00, 16.34, 18.11, 21.44 };
+        for (int x = 0; x < payRate.Length; ++x)
+            WriteLine("Pay rate {0} is {1}",
+                x, payRate[x].ToString("C"));
+    }
+}
